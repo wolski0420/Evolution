8256ff5fbaa7067661e28ca443182f8915ea220d
wolski0420 <wolski0420@gmail.com>
2020-12-20 21:30:34 +0100

    One more commit for non added file

---
+++ src/main/java/agh/edu/pl/params/Input.java
@@ -0,0 +1,10 @@
+package agh.edu.pl.params;
+
+public class Input {
+    public int width;
+    public int height;
+    public int startEnergy;
+    public int moveEnergy;
+    public int plantEnergy;
+    public double jungleRatio;
+}
